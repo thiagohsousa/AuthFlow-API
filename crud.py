fed812ef7a53d69ca1dedadf6db576e6b83eb713
Thiago <thiagohs.dev@gmail.com>
2026-03-11 19:26:31 -0300

    adicionando o Crud e o .Gitignore

---
+++ crud.py
@@ -0,0 +1,70 @@
+from sqlalchemy.orm import Session
+from models_db import Usuarios
+from schema import Usuario
+
+
+def criar_usuario(db: Session, usuario: Usuario):
+    novo_usuario = Usuarios(
+        Id=str(usuario.id),
+        Nome=usuario.nome,
+        Sobrenome=usuario.sobrenome,
+        Nome_do_Meio=usuario.nome_meio,
+        genero=usuario.genero,
+        Funcao=usuario.funcoes[0] if usuario.funcoes else None
+    )
+
+    db.add(novo_usuario)
+    db.commit()
+
+    return novo_usuario
+
+def deletar_usuario(db: Session, usuario_id):
+    usuario = db.query(Usuarios).filter(Usuarios.Id == str(usuario_id)).first()
+    if usuario:
+        db.delete(usuario)
+        db.commit()
+    else: 
+        raise ValueError("Usuario não encontrado") 
+    
+
+def listar_usuario(db : Session, usuario_id):
+    usuario = db.query(Usuarios).filter(Usuarios.Id == str(usuario_id)).first()
+    
+    if usuario:
+        return usuario
+    else:
+        raise ValueError("Usuario não encontrado")
+    
+
+def buscar_usuario_por_nome(db: Session, nome: str):
+    usuarios = db.query(Usuarios).filter(Usuarios.Nome.ilike(f"%{nome}%")).all()
+    
+    if usuarios:
+        return usuarios
+    else:
+        raise ValueError("Nenhum usuario encontrado com esse nome")
+
+
+def atualizar_usuario(db : Session, usuario_id, dados):
+    usuario = db.query(Usuarios).filter(Usuarios.Id == str(usuario_id)).first()
+    if not usuario:
+        raise ValueError("Usuario não encontrado")
+    
+    if dados.nome:
+        usuario.Nome = dados.nome
+    
+    if dados.sobrenome:
+        usuario.Sobrenome = dados.sobrenome
+
+    if dados.nome_meio:
+        usuario.Nome_do_Meio = dados.nome_meio
+
+    if dados.genero:
+        usuario.genero = dados.genero
+
+    if dados.funcoes:
+        usuario.Funcao = dados.funcoes[0]
+
+    db.commit()
+    return usuario
+
