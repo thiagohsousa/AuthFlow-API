d625d6852ab3bdb0b54bcf131da9eef8cf7ecf3f
Thiago <thiagohs.dev@gmail.com>
2026-03-28 21:46:34 -0300

    Trocando banco de dados

--- database.py
+++ database.py
@@ -1,14 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
 
 
-##conexão do MySql com o Python e sqlalchemy
+SQLALCHEMY_DATABASE_URL = "sqlite:///./users.db"
 
-DATABASE_URL = "mysql+pymysql://root:thiagohs.dev15@localhost:3306/api_usuarios"
 
-engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
